7117655164a8d708d7086c888a31bbb9c830fcb0
Pierre Thomain <pthomain@gmail.com>
2018-11-10 17:49:21 +0000

    Adding network connectivity extensions

--- app/src/main/java/uk/co/glass_software/android/boilerplate/utils/rx/RxScheduling.kt
+++ app/src/main/java/uk/co/glass_software/android/boilerplate/utils/rx/RxScheduling.kt
@@ -1,122 +1,111 @@
 package uk.co.glass_software.android.boilerplate.utils.rx
 
+
 import android.net.NetworkInfo.State.CONNECTED
 import com.github.pwittchen.reactivenetwork.library.rx2.ReactiveNetwork
 import io.reactivex.*
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.functions.Function
 import io.reactivex.schedulers.Schedulers
 import uk.co.glass_software.android.boilerplate.Boilerplate
-import uk.co.glass_software.android.boilerplate.utils.log.log
+import uk.co.glass_software.android.boilerplate.Boilerplate.logger
 import uk.co.glass_software.android.boilerplate.utils.rx.On.*
 import java.io.IOException
 import java.util.concurrent.Executor
 import java.util.concurrent.TimeoutException
 
+private val context = Boilerplate.context
 private val logger = Boilerplate.logger
+private val networkConnectivityObservable = ReactiveNetwork.observeNetworkConnectivity(context)
 
 const val MAX_NETWORK_RECONNECT_ATTEMPTS = 3
 
-fun <T> Observable<T>.ioUi(waitForNetwork: Boolean = false) =
+fun <T> Observable<T>.ioUi(waitForNetwork: Boolean = true) =
         subscribeOnIo(waitForNetwork, MainThread)
 
-fun <T> Single<T>.ioUi(waitForNetwork: Boolean = false) =
+fun <T> Single<T>.ioUi(waitForNetwork: Boolean = true) =
         subscribeOnIo(waitForNetwork, MainThread)
 
-fun <T> Maybe<T>.ioUi(waitForNetwork: Boolean = false) =
+fun <T> Maybe<T>.ioUi(waitForNetwork: Boolean = true) =
         subscribeOnIo(waitForNetwork, MainThread)
 
-fun Completable.ioUi(waitForNetwork: Boolean = false) =
+fun Completable.ioUi(waitForNetwork: Boolean = true) =
         subscribeOnIo(waitForNetwork, MainThread)
 
-fun <T> Observable<T>.subscribeOnIo(waitForNetwork: Boolean = false,
+fun <T> Observable<T>.subscribeOnIo(waitForNetwork: Boolean = true,
                                     observeOn: On = Trampoline) =
         schedule(Io, observeOn, waitForNetwork)
 
-fun <T> Single<T>.subscribeOnIo(waitForNetwork: Boolean = false,
+fun <T> Single<T>.subscribeOnIo(waitForNetwork: Boolean = true,
                                 observeOn: On = Trampoline) =
         schedule(Io, observeOn, waitForNetwork)
 
-fun <T> Maybe<T>.subscribeOnIo(waitForNetwork: Boolean = false,
+fun <T> Maybe<T>.subscribeOnIo(waitForNetwork: Boolean = true,
                                observeOn: On = Trampoline) =
         schedule(Io, observeOn, waitForNetwork)
 
 fun Completable.subscribeOnIo(waitForNetwork: Boolean = false,
-                              observeOn: On = Trampoline) =
-        schedule(Io, observeOn, waitForNetwork)
+                              observeOn: On = Trampoline) = schedule(Io, observeOn, waitForNetwork)
 
 fun <T> Observable<T>.schedule(subscribeOn: On = Io,
                                observeOn: On = MainThread,
-                               waitForNetwork: Boolean = false,
-                               maxAttempts: Int =
-                                       MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
+                               waitForNetwork: Boolean = subscribeOn == Io,
+                               maxAttempts: Int = MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
     (if (waitForNetwork) it.waitForNetwork(maxAttempts) else it)
-            .doOnError { it.log() }
+            .doOnError { logger.e(it) }
             .subscribeOn(subscribeOn.instance())
             .observeOn(observeOn.instance())
 }!!
 
 fun <T> Single<T>.schedule(subscribeOn: On = Io,
                            observeOn: On = MainThread,
-                           waitForNetwork: Boolean = false,
-                           maxAttempts: Int =
-                                   MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
+                           waitForNetwork: Boolean = subscribeOn == Io,
+                           maxAttempts: Int = MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
     (if (waitForNetwork) it.waitForNetwork(maxAttempts) else it)
-            .doOnError { it.log() }
+            .doOnError { logger.e(it) }
             .subscribeOn(subscribeOn.instance())
             .observeOn(observeOn.instance())
 }!!
 
 fun <T> Maybe<T>.schedule(subscribeOn: On = Io,
                           observeOn: On = MainThread,
-                          waitForNetwork: Boolean = false,
-                          maxAttempts: Int =
-                                  MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
+                          waitForNetwork: Boolean = subscribeOn == Io,
+                          maxAttempts: Int = MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
     (if (waitForNetwork) it.waitForNetwork(maxAttempts) else it)
-            .doOnError { it.log() }
+            .doOnError { logger.e(it) }
             .subscribeOn(subscribeOn.instance())
             .observeOn(observeOn.instance())
 }!!
 
 fun Completable.schedule(subscribeOn: On = Io,
                          observeOn: On = MainThread,
-                         waitForNetwork: Boolean = false,
-                         maxAttempts: Int =
-                                 MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
+                         waitForNetwork: Boolean = subscribeOn == Io,
+                         maxAttempts: Int = MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
     (if (waitForNetwork) it.waitForNetwork(maxAttempts) else it)
-            .doOnError { it.log() }
+            .doOnError { logger.e(it) }
             .subscribeOn(subscribeOn.instance())
             .observeOn(observeOn.instance())
 }!!
 
-fun <T> Observable<T>.waitForNetwork(maxAttempts: Int =
-                                             MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
+fun <T> Observable<T>.waitForNetwork(maxAttempts: Int = MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
     waitForNetworkAndRetry(0, maxAttempts, it)
 }!!
 
-fun <T> Single<T>.waitForNetwork(maxAttempts: Int =
-                                         MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
+fun <T> Single<T>.waitForNetwork(maxAttempts: Int = MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
     waitForNetworkAndRetry(0, maxAttempts, it.toObservable()).firstOrError()
 }!!
 
-fun <T> Maybe<T>.waitForNetwork(maxAttempts: Int =
-                                        MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
+fun <T> Maybe<T>.waitForNetwork(maxAttempts: Int = MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
     waitForNetworkAndRetry(0, maxAttempts, it.toObservable()).firstElement()
 }!!
 
-fun Completable.waitForNetwork(maxAttempts: Int =
-                                       MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
-    waitForNetworkAndRetry(0, maxAttempts,
-            it.toObservable<RxIgnore>()).ignoreElements()
+fun Completable.waitForNetwork(maxAttempts: Int = MAX_NETWORK_RECONNECT_ATTEMPTS) = compose {
+    waitForNetworkAndRetry(0, maxAttempts, it.toObservable<RxIgnore>()).ignoreElements()
 }!!
 
-fun getNetworkAvailableCompletable(checkConnectivity: Boolean = true): Completable {
-    val networkSingle = ReactiveNetwork
-            .observeNetworkConnectivity(Boilerplate.context)
-            .doOnNext {
-                if (it.state() != CONNECTED) log("Waiting for network ...")
-            }
+fun getNetworkAvailableSingle(checkConnectivity: Boolean = true): Single<RxIgnore> {
+    val networkSingle = networkConnectivityObservable
+            .doOnNext { if (it.state() != CONNECTED) log("Waiting for network...") }
             .filter { it.state() == CONNECTED }
             .firstOrError()
 
@@ -124,28 +113,31 @@ fun getNetworkAvailableCompletable(checkConnectivity: Boolean = true): Completab
         networkSingle.flatMap { _ ->
             ReactiveNetwork
                     .observeInternetConnectivity()
-                    .doOnNext { if (!it) log("Waiting for network...") }
+                    .doOnNext { if (!it) log("Waiting for connectivity...") }
                     .filter { it }
                     .firstOrError()
         }
     } else {
         networkSingle
-    }.ignoreElement().andThen { log("Network is back") }
+    }.mapIgnore().doOnSuccess { log("Network is back") }
 }
 
 private fun <T> waitForNetworkAndRetry(attempt: Int,
                                        maxAttempts: Int,
-                                       upstream: Observable<T>) =
-        getNetworkAvailableCompletable().andThen(
-                upstream.onErrorResumeNext(Function {
-                    resumeNetworkOnError(
-                            attempt + 1,
-                            maxAttempts,
-                            it,
-                            upstream
-                    )
-                })
+                                       upstream: Observable<T>,
+                                       skipCheckAtStart: Boolean = false): Observable<T> {
+    val composed = upstream.onErrorResumeNext { error: Throwable ->
+        resumeNetworkOnError(
+                attempt + 1,
+                maxAttempts,
+                error,
+                upstream
         )
+    }
+
+    return if (skipCheckAtStart && attempt == 0) composed //useful for cache to work when offline
+    else getNetworkAvailableSingle().flatMapObservable { composed }
+}
 
 private fun isNetworkError(error: Throwable) =
         error is TimeoutException || error is IOException
@@ -155,6 +147,7 @@ private fun <T> resumeNetworkOnError(attempt: Int,
                                      error: Throwable,
                                      upstream: Observable<T>): Observable<T> {
     return if (attempt >= maxAttempts) {
+        logger.e("Could not establish network connection after $attempt attempts")
         Observable.error<T>(TooManyAttemptsException(maxAttempts, error))
     } else {
         if (isNetworkError(error)) {
@@ -164,14 +157,17 @@ private fun <T> resumeNetworkOnError(attempt: Int,
                     maxAttempts,
                     upstream
             )
-        } else Observable.error<T>(error)
+        } else {
+            logger.e(error, "Caught a non-network error")
+            Observable.error<T>(error)
+        }
     }
 }
 
 class TooManyAttemptsException(maxAttempts: Int,
                                error: Throwable)
     : Exception(
-        "Too many attempts to reconnect to the network (max$maxAttempts attempts)",
+        "Too many attempts to reconnect to the network (max $maxAttempts attempts)",
         error
 )
 
