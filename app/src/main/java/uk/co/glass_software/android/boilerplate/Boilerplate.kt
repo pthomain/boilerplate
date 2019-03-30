c9c9891b5bd576cbaad222bd4d034831545c5109
Pierre Thomain <pthomain@gmail.com>
2019-03-30 11:16:13 +0000

    Remove static access to Boilerplate instance

--- app/src/main/java/uk/co/glass_software/android/boilerplate/Boilerplate.kt
+++ app/src/main/java/uk/co/glass_software/android/boilerplate/Boilerplate.kt
@@ -1,10 +1,8 @@
 package uk.co.glass_software.android.boilerplate
 
-import android.annotation.SuppressLint
 import android.content.Context
 import uk.co.glass_software.android.boilerplate.utils.log.Logger
 import uk.co.glass_software.android.boilerplate.utils.log.SimpleLogger
-import uk.co.glass_software.android.boilerplate.utils.rx.observeNetworkAvailability
 import uk.co.glass_software.android.boilerplate.utils.string.capitaliseFirst
 
 class Boilerplate(context: Context,
@@ -14,24 +12,11 @@ class Boilerplate(context: Context,
     val context = context.applicationContext
     val logger: Logger
 
-    var networkAvailable: Boolean = false
-        private set
-
     init {
         logger = SimpleLogger(
                 isDebug,
                 getLogPrefix(logPrefix),
                 context.packageName
-        )
-
-        observeNetwork()
-    }
-
-    @SuppressLint("CheckResult")
-    private fun observeNetwork() {
-        observeNetworkAvailability().subscribe(
-                { networkAvailable = it },
-                { logger.e(this, it, "An error occurred while observing the network connectivity") }
         )
     }
 
